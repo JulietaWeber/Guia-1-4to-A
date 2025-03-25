c50ed3f4f7ba6fe708ef194287ff594f467d03ab
JulietaWeber <166619703+JulietaWeber@users.noreply.github.com>
2025-03-25 12:06:52 -0300

    Variables declaradas

--- Assets/Scripts/DatosDelUsuario.cs
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DatosDelUsuario : MonoBehaviour
 {
+    public string nombreUsuario;
+    [SerializeField] int edadUsiario;
+    [SerializeField] float estaturaUsiario;
+    [SerializeField] bool esDonante;
     // Start is called before the first frame update
     void Start()
     {
