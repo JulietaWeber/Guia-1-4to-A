972f0b7578f19c48d99229dad5552435d3cd02fc
JulietaWeber <166619703+JulietaWeber@users.noreply.github.com>
2025-04-08 12:11:26 -0300

    Tema 2 IF listo

---
+++ Assets/Scripts/Orientacion.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orientacion : MonoBehaviour
+{
+    public string nombreAlumno;
+    public int añoAlumno;
+    public string orientacionAlumno;
+
+    string tic = "T";
+    string diseño = "D";
+    string gestion = "G";
+    string medios = "M";
+    string humanidades = "H";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (nombreAlumno == "")
+        {
+            Debug.Log("Error. No se ha ingresado ningún caracter");
+        }
+        else if (añoAlumno < 1 || añoAlumno > 5)
+        {
+            Debug.Log("Error. El año ingresado no está dentro del rango");
+        }
+        // orientacion == t || 
+        // orientacion != t &&
+        else if (orientacionAlumno != "T" && orientacionAlumno != "D" && orientacionAlumno != "G" && orientacionAlumno != "M" && orientacionAlumno != "H")
+        {
+            Debug.Log("Solo puede ingresar T, D, G, M o H");   
+        }
+        else if (añoAlumno < 3)
+        {
+            Debug.Log("Error. Aún estás en el ciclo básico");
+        }
+        else
+        {
+            Debug.Log("Muchas gracias " + nombreAlumno);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
